eef5e52a2768bd05525d6dfa0f6367afaece542b
wei.pw <a499940913@hotmail.com>
2025-01-05 19:19:24 +0800

    [modified]添加示例

--- hybrid-litegraph/ViewModel/NavigationViewModel.cs
+++ hybrid-litegraph/ViewModel/NavigationViewModel.cs
@@ -1,10 +1,42 @@
 ﻿namespace hybrid_litegraph.ViewModel
 {
     using CommunityToolkit.Mvvm.ComponentModel;
+    using hybrid_litegraph.bridge;
     using hybrid_litegraph.Pages;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using Wpf.Ui;
     using Wpf.Ui.Controls;
 
+    public class AddTestDemoCommand : ICommand
+    {
+        public AddTestDemoCommand()
+        {
+     
+        }
+
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            await LiteGraphBridge.LGraphBridge.createNodeByLoad(new LiteGraphNode
+            {
+               fake_id=100,
+               nodeType= "test.testjob",
+               pos= new decimal[] { 100,100},
+               title= "test.testjob",
+               uid=100
+            });
+        }
+
+ 
+    }
     public partial class NavigationViewModel : ObservableObject
     {
         [ObservableProperty]
@@ -19,6 +51,13 @@
                         Content = "Home",
                         Icon = new SymbolIcon { Symbol = SymbolRegular.Home24 },
                         TargetPageType = typeof(MainPage)
+                    },
+                                       new NavigationViewItem()
+                    {
+                        Content = "AddTestDemo",
+                        Icon = new SymbolIcon { Symbol = SymbolRegular.Add28 },
+                        TargetPageType = null,
+                        Command=new AddTestDemoCommand()
                     }
                 ];
         }
